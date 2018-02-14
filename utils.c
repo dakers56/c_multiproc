c5c38fcbd1741b3096d86833accd131773c8deec
Ubuntu <ubuntu@ip-10-206-45-37.dqa.capitalone.com>
2018-02-14 06:00:28 +0000

    can get digits of number

--- utils.c
+++ utils.c
@@ -8,12 +8,36 @@ int main(int argc, char *argv){
 	int test[] = {123, 4567, 89012};
 	int size_test = sizeof(test) / sizeof(test[0]); 
 	for(int i = 0; i < size_test; i++){
-		printf("Number of digits in %d: %d.\n", test[i], n_digits(test[i]));
+		int digs = n_digits(test[i]);
+		printf("Number of digits in %d: %d.\n", test[i], digs);
+		int * d = digits(test[i]);
+		printf("Final result of \'digits\': \n");
+		for(int j = 0; j < digs; j++){
+			printf("%d\n", d[j]);
+		}	
 	}		
+	return 0;
 }
 
 int n_digits(int a){
 	int digs = 1;
 	while((a /= 10) > 0) digs++;	
 	return digs;
+}
+
+int *digits(int a){
+	int size = n_digits(a);
+	int *digs = malloc(size);
+	if(!digs){
+		printf("Error allocating space for digits array.\n");
+		return NULL;
+	}
+	printf("Printing digits in reverse order.\n");
+	for(int i = size - 1; i >= 0 ; i--){
+		digs[--size] = a  % 10;
+		a /= 10;
+		printf("Next digit: %d.\n", digs[size]);
+	}
+	printf("\n");
+	return digs;	
 }
