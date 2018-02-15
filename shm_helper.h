96214eff20d1d6abb399629646f52b87b5080499
Ubuntu <ubuntu@ip-10-206-45-37.dqa.capitalone.com>
2018-02-15 16:04:51 +0000

    added shm helper functions

---
+++ shm_helper.h
@@ -0,0 +1,12 @@
+#ifndef SHM_HELPER_H
+#define SHM_HELPER_H
+
+#define handle_error(msg) \
+           do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+int helper_open(char *path, int oflag,  mode_t mode, int size);
+int helper_open_default(char *path, int size);
+int helper_unlink(char *path);
+
+
+#endif
