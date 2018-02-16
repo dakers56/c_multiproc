4d0a3ef7d675a4fcdc41d5299325c1f7e700f4d5
Ubuntu <ubuntu@ip-10-206-45-37.dqa.capitalone.com>
2018-02-16 00:22:20 +0000

    naming semaphores works also

--- q_shm.c
+++ q_shm.c
@@ -6,9 +6,14 @@
 #include "shm_helper.h"
 
 static char sh_name_base[] = "/Q_SHARE_OBJ_";
-static int sh_id = 1;
+static int sh_id = 0;
 
+static char sem_name_base[] =  "/Q_SHARE_OBJ_SEM_";
+static int sem_id = 0;
+
+char *name();
 char *sh_name();
+char *sem_name();
 
 int main(int argc, char ** argv){
 	return (int) new_share(0);
@@ -20,18 +25,28 @@ SHARE * new_share(int size){
 	nm = sh_name();
 	printf("Name: %s.\n", nm);
 	free(nm);
+	nm = sem_name();
+        printf("Sem name: %s.\n", nm);
+        free(nm);
 }
 	return NULL;
 }
 
-char *sh_name(){
+char *name(char *name_base, int id){
 	char *name_cp = malloc(sizeof(sh_name_base));
-	strcpy(name_cp, sh_name_base);
-	printf("sh_id: %d.\n", sh_id);
-	printf("ch_digits(sh_id++): %s.\n", ch_digits(sh_id));
-	char *digs=ch_digits(sh_id);
-	strcat(name_cp, digs);
-	free(digs);
-	sh_id++;
-	return name_cp;
+        strcpy(name_cp, name_base);
+        printf("sh_id: %d.\n", id);
+        printf("ch_digits(sh_id++): %s.\n", ch_digits(id));
+        char *digs=ch_digits(id);
+        strcat(name_cp, digs);
+        free(digs);
+        return name_cp;
 }
+
+char *sh_name(){
+	return name(sh_name_base, ++sh_id);
+}
+
+char *sem_name(){
+	return name(sem_name_base, ++sem_id);
+	}
